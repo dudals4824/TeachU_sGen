b9efbefd4ff2596f6f803ea265a359d0b520c795
dbwls878 <dbwls878@d7feaa82-bfef-0410-be52-cff2966fac4c>
2014-02-05 07:10:37 +0000

    삭제 버튼 추가

--- Teachu/src/sGen/teachu/forSettingPage/Setting.java
+++ Teachu/src/sGen/teachu/forSettingPage/Setting.java
@@ -2,24 +2,29 @@ package sGen.teachu.forSettingPage;
 
 import java.text.ParseException;
 
+import sGen.teachu.MainActivity;
 import sGen.teachu.R;
 import sGen.teachu.DTO.BabyInfoDTO;
 import sGen.teachu.database.DBBabyInfoAdapter;
 import android.app.Activity;
+import android.content.Intent;
 import android.database.SQLException;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class Setting extends Activity {
+public class Setting extends Activity implements OnClickListener {
 	LinearLayout layout_graphview, progress, test;
 	ScrollView layout_background;
 	GridLayout layout_correctionrate;
 	TextView textview_name,textview_birth;
+	Button btn_setting_delete,btn_setting_prev;
 	DBBabyInfoAdapter mBabyAdapter = new DBBabyInfoAdapter(this); 
 	BabyInfoDTO Baby = new BabyInfoDTO(); 
 
@@ -31,6 +36,7 @@ public class Setting extends Activity {
 		layout_background = (ScrollView) findViewById(R.id.layout_background);
 		layout_correctionrate = (GridLayout) findViewById(R.id.layout_correctionrate);
 		textview_name=(TextView)findViewById(R.id.textview_name);
+		btn_setting_delete=(Button)findViewById(R.id.btn_setting_delete);
 		
 		mBabyAdapter.open();
 		try {
@@ -47,5 +53,18 @@ public class Setting extends Activity {
 		
 		GraphView graphview = new GraphView(this);
 		graphview.makeGraph(this, layout_graphview);
+		
+		btn_setting_delete.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		if(v.getId()==R.id.btn_setting_delete){
+			mBabyAdapter.deleteBaby(1);
+			Toast.makeText(this, "삭제되었습니다", Toast.LENGTH_SHORT).show();
+			Intent mainActivity=new Intent(Setting.this, MainActivity.class);
+			startActivity(mainActivity);
+		}
 	}
 }
