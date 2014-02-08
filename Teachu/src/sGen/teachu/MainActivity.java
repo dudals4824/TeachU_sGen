96585aef49067f5a4e40fc3cea3111fed33a6032
dbwls878 <dbwls878@d7feaa82-bfef-0410-be52-cff2966fac4c>
2014-02-08 08:24:36 +0000

    두번누르면 종료

--- Teachu/src/sGen/teachu/MainActivity.java
+++ Teachu/src/sGen/teachu/MainActivity.java
@@ -1,69 +1,46 @@
 package sGen.teachu;
 
 import sGen.teachu.database.DBBabyInfoAdapter;
-import sGen.teachu.forSettingPage.Setting;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Handler;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_splash);
+		Handler handler = new Handler();
+		handler.postDelayed(new Runnable() {
+			
+			@Override
+			public void run() {
+				DBBabyInfoAdapter mAdapter = new DBBabyInfoAdapter(MainActivity.this);
+				mAdapter.open();
+				int babyInt = mAdapter.getBabyCount();
+
+				if (babyInt == 0) { // baby not exist
+
+					Intent AddBabyActivity = new Intent(MainActivity.this,
+							AddBaby.class);
+					startActivity(AddBabyActivity);
+				} else {
+
+					// TODO Auto-generated method stub
+					Intent CategoryTree = new Intent(MainActivity.this,
+							CategoryTree.class);
+					startActivity(CategoryTree);
+
+				}
+				mAdapter.close();
+				finish();
+			}
+		}, 3000);
 		
-		startActivity(new Intent(this, SplashActivity.class));
-		DBBabyInfoAdapter mAdapter = new DBBabyInfoAdapter(this);
-		mAdapter.open();
-
-		int babyInt = mAdapter.getBabyCount();
-
-		if (babyInt == 0) { // baby not exist
-
-			Intent AddBabyActivity = new Intent(MainActivity.this,
-					AddBaby.class);
-			startActivity(AddBabyActivity);
-		} else {
-
-			// TODO Auto-generated method stub
-			Intent CategoryTree = new Intent(MainActivity.this,
-					CategoryTree.class);
-			startActivity(CategoryTree);
-
-		}
-		mAdapter.close();
 	}
+	
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// TODO Auto-generated method stub
-		menu.add(0, 1, 0, "아기 초기화");
-		menu.add(0, 2, 0, "아기 상태보러가기");
-		return super.onCreateOptionsMenu(menu);
-	}
-
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// TODO Auto-generated method stub
-		switch (item.getItemId()) {
-		case 1:
-			// DB initializing : for testing
-			DBBabyInfoAdapter mAdapter = new DBBabyInfoAdapter(this);
-			mAdapter.open();
-			mAdapter.deleteBaby(1);
-			int babyInt = mAdapter.getBabyCount();
-			mAdapter.close();
-
-			Log.e("MINKA", "delecomplele? mAdapter.getBabyCount() = " + babyInt);
-
-		case 2:
-			Intent Setting = new Intent(MainActivity.this, Setting.class);
-			startActivity(Setting);
-		}
-		return super.onOptionsItemSelected(item);
-	}
 }
