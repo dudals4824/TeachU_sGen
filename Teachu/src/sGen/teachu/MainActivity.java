5b399e7c4e3d95e27417940c4876e16ca397ca1a
dbwls878 <dbwls878@d7feaa82-bfef-0410-be52-cff2966fac4c>
2014-02-08 13:40:21 +0000

    인텐트 수정

--- Teachu/src/sGen/teachu/MainActivity.java
+++ Teachu/src/sGen/teachu/MainActivity.java
@@ -5,42 +5,56 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	private Button btn_start;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_splash);
+		setContentView(R.layout.activity_main);
 		Handler handler = new Handler();
 		handler.postDelayed(new Runnable() {
-			
+
 			@Override
 			public void run() {
-				DBBabyInfoAdapter mAdapter = new DBBabyInfoAdapter(MainActivity.this);
+				DBBabyInfoAdapter mAdapter = new DBBabyInfoAdapter(
+						MainActivity.this);
 				mAdapter.open();
 				int babyInt = mAdapter.getBabyCount();
 
 				if (babyInt == 0) { // baby not exist
+					btn_start = (Button) findViewById(R.id.btn_main_start);
+					btn_start.setVisibility(1);
+					btn_start.setOnClickListener(new OnClickListener() {
+						@Override
+						public void onClick(View v) {
+							// TODO Auto-generated method stub
+							Intent AddBabyActivity = new Intent(
+									MainActivity.this, AddBaby.class);
+							startActivity(AddBabyActivity);
+							finish();
+						}
+					});
 
-					Intent AddBabyActivity = new Intent(MainActivity.this,
-							AddBaby.class);
-					startActivity(AddBabyActivity);
 				} else {
 
 					// TODO Auto-generated method stub
 					Intent CategoryTree = new Intent(MainActivity.this,
 							CategoryTree.class);
+					CategoryTree.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 					startActivity(CategoryTree);
-
+					finish();
 				}
 				mAdapter.close();
-				finish();
+
 			}
 		}, 3000);
-		
+
 	}
-	
 
 }
